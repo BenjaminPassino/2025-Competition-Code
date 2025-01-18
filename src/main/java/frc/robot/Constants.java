33bb6460e367bb58a241bb4d3331c8da3abcdea5
LAemmer <lillian.aemmer@gmail.com>
2025-01-18 13:42:24 -0800

    started mech code for coral, algae, and elevator

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,7 +13,44 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
+  /* MECH ITEMS */
+//LeftAlgaeMotor
+//RightAlgaeMotor
+//CoralScoringMotor
+//CoralArmMotor 
+//ElevatorMotor
+//DeepClimbMotor
+//ElevatorBottomLimitSwitch
+//DeepClimbArmLimitSwitch
+//DeepClimbCageLimitSwitch
+//CoralArmLimitSwitch
+//CoralArmEncoder
+
+  //Motor ID's
+
+  //ALGAE
+  public static final int LeftAlgaeMotorPort = 0; //TODO This will need updated
+  public static final int RightAlgaeMotorPort = 0;
+  //CORAL
+  public static final int CoralScoringMotorPort = 0;
+  public static final int CoralArmMotorPort = 0;
+  //ELEVATOR
+  public static final int ElevatorMotorPort = 0;
+  //DEEP CLIMB
+  public static final int DeepClimbMotorPort = 0;
+  
+  //Limit Switch
+  public static final int CoralArmLimitSwitchPort = 0;
+  public static final int ElevatorBottomLimitSwitchPort = 0;
+  public static final int DeepClimbArmLimitSwitchPort = 0;
+  public static final int DeepClimbCageLimitSwitchPort = 0;
+  //end of mech items
+
+
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+
+    
   }
 }
--- src/main/java/frc/robot/subsystems/ReefMechanismSubsystem.java
+++ src/main/java/frc/robot/subsystems/ReefMechanismSubsystem.java
@@ -7,51 +7,174 @@ import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class ReefMechanismSubsystem extends SubsystemBase{
+    /* Motors */
     //algae motors
-    private final PWMSparkMax LeftAlgaeMotor = new PWMSparkMax(1);
-    private final PWMSparkMax RightAlgaeMotor = new PWMSparkMax(2);
+    private final PWMSparkMax LeftAlgaeMotor = new PWMSparkMax(Constants.LeftAlgaeMotorPort);
+    private final PWMSparkMax RightAlgaeMotor = new PWMSparkMax(Constants.RightAlgaeMotorPort);
     //Coral motors
-    private final PWMSparkMax CoralScoringMotor = new PWMSparkMax(3);
-    private final PWMSparkMax CoralArmMotor = new PWMSparkMax(4);
+    private final PWMSparkMax CoralScoringMotor = new PWMSparkMax(Constants.CoralScoringMotorPort);
+    private final PWMSparkMax CoralArmMotor = new PWMSparkMax(Constants.CoralArmMotorPort);
     //Elevator motor
-    private final PWMSparkMax ElevatorMotor = new PWMSparkMax(5);
+    private final PWMSparkMax ElevatorMotor = new PWMSparkMax(Constants.ElevatorMotorPort);
     // Deep Climb
-    private final PWMSparkMax DeepClimbMotor = new PWMSparkMax(6);
-    // limit switch
-    private final DigitalInput ElevatorBottomLimitSwitch = new DigitalInput(0);
-    private final DigitalInput DeepClimbArmLimitSwitch = new DigitalInput(1);
-    private final DigitalInput DeepClimbCageLimitSwitch = new DigitalInput(2);
-    //encoder
+    private final PWMSparkMax DeepClimbMotor = new PWMSparkMax(Constants.DeepClimbMotorPort);
+    //deep climb servo motor
+
+    /* Limit Switch */
+    private final DigitalInput ElevatorBottomLimitSwitch = new DigitalInput(Constants.ElevatorBottomLimitSwitchPort);
+    private final DigitalInput DeepClimbArmLimitSwitch = new DigitalInput(Constants.DeepClimbArmLimitSwitchPort);
+    private final DigitalInput DeepClimbCageLimitSwitch = new DigitalInput(Constants.DeepClimbCageLimitSwitchPort);
+    private final DigitalInput CoralArmLimitSwitch = new DigitalInput(Constants.CoralArmLimitSwitchPort);
+
+    /* Encoders */
     private final Encoder CoralArmEncoder = new Encoder(3, 4);
 
-    public Command mechanismInitializeCommand() {
+   // public Command mechanismInitializeCommand() {
         // Inline construction of command goes here.
         // Subsystem::RunOnce implicitly requires `this` subsystem.
-        return runOnce(
-            () -> {
-              /* one-time action goes here */
-              
-            });
+        
+  //    }
+
+      /* CONTROLS */
+
+
+// call buttons here
+// learn by 1/21
+
+/* CORAL MECHANISM */
+      //score coral
+      public void CoralScoringMethod() { //TODO figure out how to call
+        if (ArmScoringPosition())
+        {
+            CoralScoringMotor.set(.4);
+        }
       }
 
+      //get arm into scoring position
+      public Boolean ArmScoringPosition() {
+        if (CoralArmLimitSwitch.get())
+        {
+            CoralArmMotor.set(0); //do not know until motor is mounted
+        }
+        else
+        {
+            CoralArmMotor.set(.2); // TODO do not know
+        }
+        return CoralArmLimitSwitch.get();
+      }
+
+      //intake coral
+      public void CoralCollectionMethod() { //TODO figure out how to call
+        if (ArmCollectionPosition())
+        {
+            CoralScoringMotor.set(.4);
+        }
+    }
+
+      //collect coral from station
+      public Boolean ArmCollectionPosition() {
+        if (CoralArmLimitSwitch.get())
+        {
+            CoralArmMotor.set(0); //do not know until motor is mounted
+        }
+        else
+        {
+            CoralArmMotor.set(.2); // TODO do not know
+        }
+        return CoralArmLimitSwitch.get();
+      }
+
+
+
+ /* ALGAE MECHANISM */
+      //collect algae
+      public void AlgaeCollectionMethod()
+      {
+        AlgaeMotorControl(.2);
+      }
+      
+      //score algae (processor)
+      public void AlgaeScoringMethod()
+      {
+        AlgaeMotorControl(-0.2);
+      }
+
+        //control both algae motors
+      public void AlgaeMotorControl(double speed)
+      {
+        LeftAlgaeMotor.set(-speed); //TODO check motor direction
+        RightAlgaeMotor.set(speed);
+      }
+
+
+/* ELEVATOR */
+      //UP
+      //TODO: Find elevator heights for all four reef levels and processor (5 total)
+      public void ElevatorUpMethod()
+      {
+        ElevatorMotor.set(.2); //TODO find speed for elevator
+      }
+      //DOWN
+      public void ElevatorDownMethod()
+      // elevator check method
+      {
+        ElevatorMotor.set(-0.2); //opposite of elevator up (so elevator goes down)
+      }
+
+      //STOP
+      public void ElevatorStopMethod() // may not need 
+      {
+
+      }
+
+      //CHECKING IF ELEVATOR IS AT BOTTOM
+      public boolean ElevatorPosition() // do not know if this works
+      {
+        if (ElevatorBottomLimitSwitch.get())
+        {
+            ElevatorMotor.set(0);
+        }
+        else 
+        {
+            ElevatorMotor.set(.2);
+        }
+        return ElevatorBottomLimitSwitch.get();
+      }
+      
+
+/* DEEP CLIMB */
+
+// press buttom
+// check position
+// grab on to cage
+// lift robot
+// activate limit switch and stop
 }
 
 // initialize components
 
 
 
+/* Motors */
 //LeftAlgaeMotor
 //RightAlgaeMotor
 //CoralScoringMotor
 //CoralArmMotor 
-//CoralArmEncoder
 //ElevatorMotor
-//ElevatorBottomLimitSwitch
-//DeepClimbArmLimitSwitch
-//DeepClimbCageLimitSwitch
 //DeepClimbMotor
+// servo motor for deep climb
+
+/* Limit Switches */
+//ElevatorBottomLimitSwitch
+//DeepClimbArmLimitSwitch (prevent arm from hitting robot)
+//DeepClimbCageLimitSwitch (sucessfully grab cage)
+//CoralArmLimitSwitch
+
+/* Encoders */
+//CoralArmEncoder
 
 // limit switch or encoder for coral
 // four settings for elevator height
