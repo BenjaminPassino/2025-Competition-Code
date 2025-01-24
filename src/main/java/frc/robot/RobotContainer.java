e49e56a703ae0d02fe4fc495a55538341ffc5a2d
Ellie <ejw70927@gmail.com>
2025-01-23 20:08:34 -0500

    drive test

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -24,7 +24,7 @@ public class RobotContainer {
 
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-            .withDeadband(MaxSpeed * 0.3).withRotationalDeadband(MaxAngularRate * 0.3) // Add a 10% deadband
+            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
             .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
     private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
     private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
@@ -45,9 +45,9 @@ public class RobotContainer {
         drivetrain.setDefaultCommand(
             // Drivetrain will execute this command periodically
             drivetrain.applyRequest(() ->
-                drive.withVelocityX(slowspeed(-joystick.getLeftY() * MaxSpeed)) // Drive forward with negative Y (forward)
-                    .withVelocityY(slowspeed(-joystick.getLeftX() * MaxSpeed )) // Drive left with negative X (left)
-                    .withRotationalRate(slowspeed(-joystick.getRightX() * MaxAngularRate )) // Drive counterclockwise with negative X (left)
+                drive.withVelocityX(slowspeed(-joystick.getLeftY() * MaxSpeed *.5)) // Drive forward with negative Y (forward)
+                    .withVelocityY(slowspeed(-joystick.getLeftX() * MaxSpeed* .5)) // Drive left with negative X (left)
+                    .withRotationalRate(slowspeed(-joystick.getRightX() * MaxAngularRate *.5 )) // Drive counterclockwise with negative X (left)
                     )
         );
  
